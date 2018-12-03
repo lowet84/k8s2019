1ac5fe66466fd3680b01fdc5acee60270facda96
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2018-12-03 13:51:02 +0100

    Color generation

--- demo/server/server.ts
+++ demo/server/server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 var app = express()
  
-var number = Math.random(); 
+var number = '#'+Math.floor(Math.random()*16777215).toString(16);
 
 app.get('/', function (req, res) {
   res.send(`${number}`)
