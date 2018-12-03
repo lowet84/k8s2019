634c450147e111ca9a4db87a06081e05ccb2a7a3
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-12-03 06:34:22 +0000

    GIT :/

--- port-forward/start.sh
+++ port-forward/start.sh
@@ -3,5 +3,5 @@
 docker build -t port-forward .
 docker rm -f port-forward-80 port-forward-443
 
-docker run --name port-forward-80 -p 80:1234 -e PORT=30080 -d port-forward
-docker run --name port-forward-443 -p 443:1234 -e PORT=30443 -d port-forward
+docker run --name port-forward-80 --net=host -e TO=30080 -e FROM=80 -d port-forward
+docker run --name port-forward-443 --net=host -e TO=30443 -e FROM=30443 -d port-forward
