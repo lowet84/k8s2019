53060e161e6cb34b747aac3ac687d7e6400cbf1c
root <root@Ubuntu.asdu3tqgoxnuzcsg1dc3czbidb.fx.internal.cloudapp.net>
2018-12-03 10:29:37 +0000

    Don't push this commit

--- port-forward/start.sh
+++ port-forward/start.sh
@@ -4,4 +4,4 @@ docker build -t port-forward .
 docker rm -f port-forward-80 port-forward-443
 
 docker run --name port-forward-80 --net=host -e TO=30080 -e FROM=80 -d port-forward
-docker run --name port-forward-443 --net=host -e TO=30443 -e FROM=30443 -d port-forward
+docker run --name port-forward-443 --net=host -e TO=30443 -e FROM=443 -d port-forward
