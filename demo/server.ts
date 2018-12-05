8075a290cec71bf062d620776456c5fc9095aab9
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2018-12-05 11:33:48 +0100

    Added gzip compression

--- demo/server.ts
+++ demo/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import compression from 'compression'
+
 var app = express()
+app.use(compression())
  
 var number = '#'+Math.floor(Math.random()*16777215).toString(16);
 
