8c233e2283bd91cfa8ca32dc0d7af94627a5f671
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2018-12-05 13:05:13 +0100

    Fixed builds and stuff

--- demo/client/index.ts
+++ demo/client/index.ts
@@ -1,10 +1,20 @@
-import { html, render, TemplateResult } from 'lit-html';
+import { html, render, TemplateResult } from 'lit-html'
 
 const root: TemplateResult = html`
-  <h1>Well, hello there!</h1>
-`;
+  <div class="root"><h1 class="test">Well, hello there!</h1></div>
+`
 
 window.onload = () => {
-    const el = document.body;
-    render(root, el);
+  const el = document.body
+  render(root, el)
 }
+
+var test = async (path: string) => {
+  var url = (window['api'] || '') + path
+  console.log(url)
+  var value = await fetch(url).catch(error => console.log(error))
+  if (!value) return
+  console.log(await value.text())
+}
+
+test('/color')
--- demo/server.ts
+++ demo/server.ts
@@ -3,7 +3,15 @@ import compression from 'compression'
 
 var app = express()
 app.use(compression())
- 
+
+if(process.argv[2] === 'dev'){
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+}
+
 var number = '#'+Math.floor(Math.random()*16777215).toString(16);
 
 app.get('/color', function (req, res) {
