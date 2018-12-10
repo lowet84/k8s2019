fd9c8c54b2ab685f0fa4bb015ada118eac609373
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2018-12-10 17:06:10 +0100

    Updating color background

--- demo/client/index.ts
+++ demo/client/index.ts
@@ -1,20 +1,20 @@
 import { html, render, TemplateResult } from 'lit-html'
 
 const root: TemplateResult = html`
-  <div class="root"><h1 class="test">Well, hello there!</h1></div>
+  <div id="root" class="root"></div>
 `
 
 window.onload = () => {
   const el = document.body
   render(root, el)
+  test('/api')
 }
 
 var test = async (path: string) => {
   var url = (window['api'] || '') + path
-  console.log(url)
   var value = await fetch(url).catch(error => console.log(error))
   if (!value) return
-  console.log(await value.text())
+  var root = document.getElementById('root')
+  var data = await value.json()
+  root.style.backgroundColor = `rgb(${data.color.r},${data.color.g},${data.color.b})`
 }
-
-test('/color')
--- demo/server.ts
+++ demo/server.ts
@@ -4,20 +4,42 @@ import compression from 'compression'
 var app = express()
 app.use(compression())
 
-if(process.argv[2] === 'dev'){
+if (process.argv[2] === 'dev') {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
 }
 
-var number = '#'+Math.floor(Math.random()*16777215).toString(16);
+var id = Math.floor(Math.random() * 16777215)
+var r = Math.floor(Math.random() * 255)
+var g = Math.floor(Math.random() * 255)
+var b = Math.floor(Math.random() * 255)
 
-app.get('/color', function (req, res) {
-  res.send(`${number}`)
+var updateColor = function(){
+  if(r > 0 && b == 0){
+    r--;
+    g++;
+  }
+  if(g > 0 && r == 0){
+    g--;
+    b++;
+  }
+  if(b > 0 && g == 0){
+    r++;
+    b--;
+  }
+}
+
+app.get('/api', function(req, res) {
+  updateColor()
+  res.send({ id, color: { r, g, b } })
 })
- 
+
 app.use(express.static('web'))
 
 app.listen(3000)
