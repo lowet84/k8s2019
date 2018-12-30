2bcbd01bfbe5e55b43a73dd5f362e891987c7ada
Fredrik Löwenhamn <lowet84@gmail.com>
2018-12-31 00:05:50 +0100

    Added images

--- presentation/src/pages/0.root.ts
+++ presentation/src/pages/0.root.ts
@@ -1,15 +1,10 @@
 import { html } from '../../node_modules/lit-html'
 import setup from './10.setup'
+import ego from './20.ego'
 
 const page = html`
   ${setup}
-  <section>
-    <section>Vertical 1</section>
-    <section>
-      <div>Vertical 2</div>
-      <div>Subtitle</div>
-    </section>
-  </section>
+  ${ego}
 `
 
 export default page
---
+++ presentation/src/pages/20.ego.ts
@@ -0,0 +1,22 @@
+import { html } from '../../node_modules/lit-html'
+
+const page = html`
+  <section>
+    <section>
+      <h1>Docker & Kubernetes</h1>
+      <h3>Fredrik Löwenhamn</h3>
+    </section>
+    <section>
+      <div>This guy?</div>
+      <img src="../assets/ego.jpg" class="image-small" />
+      <div class="image-bar">
+        <img src="../assets/dotnet.svg" class="image-small" />
+        <img src="../assets/docker.svg" class="image-small" />
+        <img src="../assets/k8s.png" class="image-small" />
+        <img src="../assets/avega.png" class="image-small" />
+      </div>
+    </section>
+  </section>
+`
+
+export default page
