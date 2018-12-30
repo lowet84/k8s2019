e08eb766f6437c3b5b867dc0d0d167ffd83fc1f0
Fredrik Löwenhamn <lowet84@gmail.com>
2018-12-31 00:14:14 +0100

    Worked on slides

--- presentation/src/pages/0.root.ts
+++ presentation/src/pages/0.root.ts
@@ -1,10 +1,12 @@
 import { html } from '../../node_modules/lit-html'
 import setup from './10.setup'
 import ego from './20.ego'
+import docker from './30.docker'
 
 const page = html`
   ${setup}
   ${ego}
+  ${docker}
 `
 
 export default page
---
+++ presentation/src/pages/30.docker.ts
@@ -0,0 +1,18 @@
+import { html } from '../../node_modules/lit-html'
+
+const page = html`
+  <section>
+    <section>
+        <h2>Docker</h2>
+        <img src="../assets/docker.svg" class="image-small" />
+    </section>
+    <section>
+        <h2>Två familjer av problem</h2>
+        <div class="fragment"></div>
+        <div class="fragment"></div>
+    </section>
+    
+  </section>
+`
+
+export default page
