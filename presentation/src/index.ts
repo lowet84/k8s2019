7a0fe32d50600a37474c56c43fdadb683a1be5f2
Fredrik Löwenhamn <lowet84@gmail.com>
2018-12-30 18:54:44 +0100

    Presentaton now works

--- presentation/src/index.ts
+++ presentation/src/index.ts
@@ -1,4 +1,11 @@
+import { render } from '../node_modules/lit-html'
+import root from './components/root'
+
+
 document.addEventListener('DOMContentLoaded', function(event) {
+  const el = document.getElementById("slides")
+  render(root, el)
+
   const reveal = require('reveal.js/js/reveal')
   reveal.initialize()
 })
