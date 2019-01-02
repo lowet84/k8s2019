40c6e8bbc795495c1d465c5f5aec7afe143ce203
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2019-01-02 14:10:14 +0100

    Added button

--- presentation/src/pages/10.setup.ts
+++ presentation/src/pages/10.setup.ts
@@ -2,19 +2,11 @@ import { html } from '../../node_modules/lit-html'
 
 const page = html`
   <section>
-    <div>Setup</div>
-
-    <button
-      class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
-    >
-      Load key
-    </button>
-    <form action="#">
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label dark">
-        <input class="mdl-textfield__input" type="text" id="sample3" />
-        <label class="mdl-textfield__label" for="sample3">Server url</label>
-      </div>
-    </form>
+    <section><h3>Setup</h3></section>
+    <section>
+      <div>Ssh private key</div>
+      <div><button class="button">Select key</button></div>
+    </section>
   </section>
 `
 
