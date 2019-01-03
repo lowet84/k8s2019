516d8032834589f019d8e0c4c2cab0b816e6cdf7
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2019-01-03 10:58:50 +0100

    Moved everything to html file

--- presentation/src/index.ts
+++ presentation/src/index.ts
@@ -1,10 +1,4 @@
-import { render, html, TemplateResult } from 'lit-html'
-import root from './pages/0.root'
-
 document.addEventListener('DOMContentLoaded', function(event) {
-  const el = document.getElementById("slides")
-  render(root, el)
-
   const reveal = require('reveal.js/js/reveal')
   reveal.initialize()
 })
