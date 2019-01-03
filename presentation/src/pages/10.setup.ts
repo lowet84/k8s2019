798f66f15893d8e34ec0ae9b3682877f8d57597e
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2019-01-03 10:41:55 +0100

    Removed unused stuff

--- presentation/src/pages/10.setup.ts
+++ presentation/src/pages/10.setup.ts
@@ -1,12 +1,4 @@
 import { html } from 'lit-html'
-import { existsSync, readFileSync, writeFileSync } from 'fs'
-
-const filename = 'settings.json'
-const loadSettings = () => {
-  console.log('loading settings')
-}
-
-var data: { username: string }
 
 const page = html`
   <section>
