74673138050dd77b93d233c5c9fc550031246df6
Fredrik Löwenhamn <fredrik.lowenhamn@beijerelectronics.com>
2019-01-04 15:26:12 +0100

    Vue is back baby!

---
+++ presentation/src/setup/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <button onclick="{window.location.href='./slides.html'}">Back</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-typed'
+
+@Component()
+export default class extends Vue {
+  msg= 'some message'
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
