a94f16123b0d80124d49ace3635cc817e413f56a
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-07 14:23:58 +0000

    work in progress

--- setup-hypriot.sh
+++ setup-hypriot.sh
@@ -20,7 +20,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 
-apt-get install -y kubeadm
+apt-get install -y kubeadm docker-ce=18.06.1~ce~3-0~raspbian
 
 echo "runcmd:" >> /boot/device-init.yaml
 echo "  - sudo iptables -A FORWARD -i cni0 -j ACCEPT" >> /boot/device-init.yaml
