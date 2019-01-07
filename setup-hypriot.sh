24dfc48fba83f3c6641b5d59f46d69821cf2a3fb
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-07 10:48:04 +0000

    what the hell happened here

---
+++ setup-hypriot.sh
@@ -0,0 +1,29 @@
+apt-get update
+apt-get install -y netfilter-persistent
+mkdir -p /etc/iptables/
+
+echo "*filter
+:KUBE-SERVICES - [0:0]
+-A FORWARD -i cni0 -j ACCEPT
+-A FORWARD -o cni0 -j ACCEPT
+-A FORWARD -i flannel.1 -j ACCEPT
+-A FORWARD -o flannel.1 -j ACCEPT
+COMMIT
+" | sudo tee -a /etc/iptables/rules.v4 > /dev/null
+
+sudo iptables -A FORWARD -i cni0 -j ACCEPT
+sudo iptables -A FORWARD -o cni0 -j ACCEPT
+sudo iptables -A FORWARD -i flannel.1 -j ACCEPT
+sudo iptables -A FORWARD -o flannel.1 -j ACCEPT
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+
+apt-get install -y kubeadm
+
+echo "runcmd:" >> /boot/device-init.yaml
+echo "  - sudo iptables -A FORWARD -i cni0 -j ACCEPT" >> /boot/device-init.yaml
+echo "  - sudo iptables -A FORWARD -o cni0 -j ACCEPT" >> /boot/device-init.yaml
+echo "  - sudo iptables -A FORWARD -i flannel.1 -j ACCEPT" >> /boot/device-init.yaml
+echo "  - sudo iptables -A FORWARD -o flannel.1 -j ACCEPT" >> /boot/device-init.yaml
