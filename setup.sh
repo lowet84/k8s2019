c6d442e812e7e13845a5abb62fc2ff107a865fed
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-07 10:45:10 +0000

    First Blood

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-kubeadm init
-mkdir -p /root/.kube
-cp /etc/kubernetes/admin.conf /root/.kube/config
+sudo kubeadm init
+mkdir -p ~/.kube
+cp /etc/kubernetes/admin.conf ~/.kube/config
 
 # Taint master
-kubectl taint nodes --all node-role.kubernetes.io/master-
+sudo kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # Weave network layer
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # Traefik load balancer
-kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-rbac.yaml
+#sudo kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-rbac.yaml
 
