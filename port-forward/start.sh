ef9ddb197b6f5f1bd6dcd5cf6ce6417b274073b5
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-08 07:57:24 +0000

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL

--- port-forward/start.sh
+++ port-forward/start.sh
@@ -3,5 +3,5 @@
 #docker build -t port-forward .
 docker rm -f port-forward-80 port-forward-443
 
-docker run --name port-forward-80 --net=host -e TO=30080 -e FROM=80 -d port-forward
-docker run --name port-forward-443 --net=host -e TO=30443 -e FROM=443 -d port-forward
+docker run --name port-forward-80 --restart=always --net=host -e TO=30080 -e FROM=80 -d port-forward
+docker run --name port-forward-443 --restart=always --net=host -e TO=30443 -e FROM=443 -d port-forward
