808a43c706f6c4405b4c9646a580d91365b35b05
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-08 08:58:37 +0000

    tagging release w.t.f.

--- setup-kubernetes-first.sh
+++ setup-kubernetes-first.sh
@@ -6,7 +6,7 @@ sudo cp /etc/kubernetes/admin.conf ~/.kube/config
 sudo chown $USER ~/.kube/config
 
 # Taint master
-kubectl taint nodes --all node-role.kubernetes.io/master-
+#kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # Weave network layer
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
