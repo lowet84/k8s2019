c92c4a2316ef675e41a30f984b0117a046087bf1
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2019-01-08 10:58:28 +0000

    hacky sack

--- setup-kubernetes-first.sh
+++ setup-kubernetes-first.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo kubeadm init
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p ~/.kube
 rm ~/.kube/config
 sudo cp /etc/kubernetes/admin.conf ~/.kube/config
@@ -11,5 +11,5 @@ sudo chown $USER ~/.kube/config
 
 # Weave network layer
 #kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-kubectl apply -f weave.yml
+kubectl apply -f kube-flannel.yml
 
--- traefik/start.sh
+++ traefik/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl apply -f service-account.yml
 kubectl apply -f secret.yaml
-kubectl apply -f deployment.yml
+kubectl apply -f ds.yml
 kubectl apply -f service.yml
 kubectl apply -f ingress.yml
 
